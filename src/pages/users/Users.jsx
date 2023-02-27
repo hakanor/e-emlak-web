54a8cbae6e6615e9e8ccaa74480fc9a28a111d87
Your Name <hakanor99@gmail.com>
2023-02-27 23:39:44 +0300

    Fix UsersPage

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -4,14 +4,36 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./users.css";
 
+import { useState, useEffect } from "react";
+import FirebaseService from '../../FirebaseService';
+
+
+
 
 const Users = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+          const collectionName = "users";
+          const result = await FirebaseService.getAll(collectionName);
+          console.log(`Fetched ${result.length} documents`);
+          setData(result);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    fetchData();
+  
+  }, []);
+
     return (
         <div className="users">
           <Sidebar />
           <div className="usersContainer">
           <Navbar />
-          <Datatable/>
+          <Datatable data={data} dataType={"user"}/>
           </div>
         </div>
       );
