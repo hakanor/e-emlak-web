fb8d2d6324bc1463867f07efc061abfc7b5acc8b
Your Name <hakanor99@gmail.com>
2023-02-27 15:22:24 +0300

    Fetch user data

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,10 +2,55 @@ import "./datatable.css";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setData(list);
+        console.log(list);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+
+    // LISTEN (REALTIME)
+    // const unsub = onSnapshot(
+    //   collection(db, "users"),
+    //   (snapShot) => {
+    //     let list = [];
+    //     snapShot.docs.forEach((doc) => {
+    //       list.push({ id: doc.id, ...doc.data() });
+    //     });
+    //     setData(list);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+
+    // return () => {
+    //   unsub();
+    // };
+  }, []);
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
